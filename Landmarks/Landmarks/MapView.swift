d290e32da3d5d2908bc8611dc035adf9ecbf2cd2
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-03-28 11:02:28 +0900

    섹션 5
    MapKit 임포트
    지도 기능 사용 - 작동안함

--- Landmarks/Landmarks/MapView.swift
+++ Landmarks/Landmarks/MapView.swift
@@ -10,7 +10,15 @@ import MapKit
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(initialPosition: .region(region))
+    }
+    
+    // 지도의 지역 정보를 보유하는 변수 선언
+    private var region: MKCoordinateRegion{
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
