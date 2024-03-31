b8b93cc449f96b0ac4debfc6cd9f244b9f0ce363
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-03-31 11:24:03 +0900

    리스트 세부정보 탐색 제작중

--- SwiftUI essentials/Introducing SwiftUI/Landmarks/Views/ContentView.swift
+++ SwiftUI essentials/Introducing SwiftUI/Landmarks/Views/ContentView.swift
@@ -9,40 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading){
-                Text("Turtle Rock")
-                    .font(.title)
-                    .foregroundStyle(.black)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-            // 폰트
-            .font(.subheadline)
-            // 배경색
-            .foregroundColor(.secondary)
-            
-            Divider()
-            
-            Text("About Turtle Rock")
-                .font(.title)
-            Text("Descriptive text goes here.")
-        }
-        .padding()
-        
-        Spacer()
-        }
+        LandmarkList()
+//        VStack {
+//            MapView()
+//                .frame(height: 300)
+//            CircleImage()
+//                .offset(y: -130)
+//                .padding(.bottom, -130)
+//            
+//            VStack(alignment: .leading){
+//                Text("Turtle Rock")
+//                    .font(.title)
+//                    .foregroundStyle(.black)
+//                
+//                HStack {
+//                    Text("Joshua Tree National Park")
+//                        .font(.subheadline)
+//                    Spacer()
+//                    Text("California")
+//                        .font(.subheadline)
+//                }
+//            // 폰트
+//            .font(.subheadline)
+//            // 배경색
+//            .foregroundColor(.secondary)
+//            
+//            Divider()
+//            
+//            Text("About Turtle Rock")
+//                .font(.title)
+//            Text("Descriptive text goes here.")
+//        }
+//        .padding()
+//        
+//        Spacer()
+//        }
     }
 }
 
