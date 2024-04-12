f74a7e0dd5df767a2bc710049cd7a019b68e6d8b
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-04-13 02:40:27 +0900

    NC1_DailyApp_Start

---
+++ SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/NC1_DailyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_DailyAppApp.swift
+//  NC1_DailyApp
+//
+//  Created by lovehyun95 on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1_DailyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
