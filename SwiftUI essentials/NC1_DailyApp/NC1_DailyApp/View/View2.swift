b60d4d818608008f6b0826b17969820b7444abf5
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-04-19 09:01:22 +0900

    휠피커랑 버튼 공존이 안됌

--- SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/View2.swift
+++ SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/View2.swift
@@ -15,20 +15,22 @@ struct View2: View {
     @Binding var indexBasic: Int
     
    var body: some View {
-        ScrollView{
+       ScrollView(.vertical, showsIndicators: false){
             Spacer()
                 // 위쪽 여백
                 .frame(height: 60)
             // 제일 처음 보여주는 개체 - 바인딩 된 Int형, 전체 요소 items = 바인딩 된 배열 형태
             SwiftUIWheelPicker($indexBasic, items: DateSetArray.dayOfWeek) { value in
-                    VStack{
-                        Text("\(value)")
-                                .frame(width: 45, height: 14, alignment: .center)
-                        Circle()
-                            .fill(Color.gray)
-                            .frame(width: 45 , height: 45)
+                            VStack{
+                                Text("\(value)")
+                                    .frame(width: 45, height: 14, alignment: .center)
+                                Circle()
+                                    .fill(Color.gray)
+                                    .frame(width: 45 , height: 45)
+                            
+                        }
                 }
-            }
+            
             .frame(width: 371, height: 30, alignment: .center)
             .padding(.top, 10)
         }.onAppear(){
