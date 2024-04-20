e6cf57aa06972d23f007fa3bfe098a30f7e26433
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-04-20 21:56:19 +0900

    횡스크롤 구현
    시점 고정하는 방법 찾아야함

--- SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/View2.swift
+++ SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/View2.swift
@@ -19,25 +19,38 @@ struct View2: View {
             Spacer()
             // 위쪽 여백
                 .frame(height: 60)
-            // 제일 처음 보여주는 개체 - 바인딩 된 Int형, 전체 요소 items = 바인딩 된 배열 형태
-            SwiftUIWheelPicker($indexBasic, items: DateSetArray.dayOfWeek) { value in
-//                Button {
-//
-//                } label: {
-                // 버튼을 추가하면 옆으로 넘어가는 기능이 작동을 하지 않음
-                    VStack{
-                        Text("\(value)")
-                            .frame(width: 45, height: 14, alignment: .center)
-                        Circle()
-                            .fill(Color.gray)
-                            .frame(width: 45 , height: 45)
+            // 스크롤 뷰가 최상단으로 나와야 여러개의 스크롤 뷰가 생기지않고 하나의 뷰만 생성 된다.
+            ScrollView(.horizontal, showsIndicators: false){
+                // 가로로 버튼을 펼치기 위해
+                HStack{
+                    // 반복문 돌면서 3년전 ~ 2달 뒤 날짜까지 불러오기 위해
+                    ForEach(DateSetArray.dateArray.indices, id: \.self) { index in
+                        Button(action: {
+                            
+                        }) {
+                            VStack {
+                                Text(DateSetArray.dateArray[index])
+                                    .frame(width: 45, height: 14, alignment: .center)
+                                Circle()
+                                    .fill(Color.gray)
+                                    .frame(width: 45 , height: 45)
+                            }
+                        }
                     }
-                    
-//                }
-                
+                }
             }
-            
-            
+//            // 제일 처음 보여주는 개체 - 바인딩 된 Int형, 전체 요소 items = 바인딩 된 배열 형태
+//            SwiftUIWheelPicker($indexBasic, items: DateSetArray.dayOfWeek) { value in
+//                // 버튼을 추가하면 옆으로 넘어가는 기능이 작동을 하지 않음
+//                    VStack{
+//                        Text("\(value)")
+//                            .frame(width: 45, height: 14, alignment: .center)
+//                        Circle()
+//                            .fill(Color.gray)
+//                            .frame(width: 45 , height: 45)
+//                    }
+////                }
+//            }
             .frame(width: 371, height: 30, alignment: .center)
             .padding(.top, 10)
         }.onAppear(){
