b82a61daa6a45ad80d8d421484f57bb0db8a98bb
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-04-28 21:39:22 +0900

    모델 정리

--- SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/Model/DateFormatCollect.swift
+++ SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/Model/DateFormatCollect.swift
@@ -6,34 +6,42 @@
 //
 
 import Foundation
-let Day = "yyyy-MM-dd"
-let monthAndDay = "MM월 dd일"
-let dayOfWeekFormatter = "EEEE"
-let koreaLanguage = "ko_KR"
 
-class DateFormatCollect{
-    // 날짜를 문자열로 바꿔주기 위한 작업
-    func DayForMatter(DayFormat : String) -> String{
-        let DayFormatter = DateFormatter()
+let Day = "yyyy-MM-dd" // 일반 날짜 포맷
+let monthAndDay = "MM월 dd일" // 월,일 표시 포맷
+let dayOfWeekFormatter = "EEEE" // 요일 표시 포맷
+let today = Date()
+
+// 추후 언어 추가할 때 사용
+enum languageList : String, CaseIterable{
+    case koreaLanguage = "ko_KR" // 한글 표시 포맷
+}
+
+struct DateFormatController{
+    let DayFormatter = DateFormatter()
+    // 날짜를 문자열로 바꿔주기 위한 작업 문자열과 날짜 반환
+    func DateController(DayFormat : String) -> String{
+        //DateFormatter 설정
+        // 기본 형식일 경우
         if DayFormat == Day{
             DayFormatter.dateFormat = Day
             
+            return DayFormat
         }
+        // 월, 일 일 경우
         else if DayFormat == monthAndDay{
             DayFormatter.dateFormat = monthAndDay
-            DayFormatter.locale = Locale(identifier:koreaLanguage)
+            DayFormatter.locale = Locale(identifier:languageList.koreaLanguage.rawValue)
             
+            return DayFormat
         }
+        // 요일 표시일 경우
         else if DayFormat == dayOfWeekFormatter{
-            DayFormatter.dateFormat = "EEEE"
-            DayFormatter.locale = Locale(identifier:koreaLanguage)
+            DayFormatter.dateFormat = dayOfWeekFormatter
+            DayFormatter.locale = Locale(identifier:languageList.koreaLanguage.rawValue)
             
+            return DayFormat
         }
-        // 월 일 표시 포맷
-        //    let monthAndDayFormatter = DateFormatter()
-        // 요일 표시 포맷
-        // 현재 날짜를 불러올 상수
-        let today = Date()
         // 3년전 날짜 구하는 형식
         let threeYearsAgo = Calendar.current.date(byAdding:.year, value: -3, to: today)
         // let _ = print("\(threeYearsAgo!.description)")
@@ -42,16 +50,71 @@ class DateFormatCollect{
         // let _ = print("\(twoMonthAfter!.description)")
         // 비교하기 위해 시작 지점과 끝 지점 계산
         // 2021-04-18 06:51:12 +0000 3년전
-        let beforeDate = DayFormatter.date(from: DayFormatter.string(from: threeYearsAgo!))
-        // 2024-06-18 06:51:12 +0000 2달 뒤
-        let afterDate = DayFormatter.date(from: DayFormatter.string(from: twoMonthAfter!))
-        let interval = afterDate?.timeIntervalSince(beforeDate!)
+       
+        let interval = threeYearsAgo?.timeIntervalSince(twoMonthAfter!)
         // 하루 기준으로 표시한 날짜 차이 - 배열의 크기가 되어야함
         let daysInterval = Int(interval! / 86400) // 1157
         // let _ = print(afterDate)
         // 변수 따로 만들어서 데이터를 갱신
         var currentDate = Calendar.current.date(byAdding: .year, value: -3, to: today)!
         // 제일 과거 날짜를 기준으로 하루씩 늘려가면서 배열에 추가
+        for _ in 0..<daysInterval {
+            // Date 배열 저장
+            DateSetArray.dateArray.append(currentDate)
+            // let _ = print(DateSetArray.dateArray)
+            // MM월dd 로 데이터 변환
+            // let _ = print(DateSetArray.dayOfWeek)
+
+            // 하루 씩 늘려가면서 날짜를 증가시킴
+            currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)!
     }
 
 }
+
+               
+}
+//            // 날짜를 문자열로 바꿔주기 위한 작업
+//            let DayFormatter = DateFormatter()
+//            DayFormatter.dateFormat = "yyyy-MM-dd"
+//            // 월 일 표시 포맷
+//            let monthAndDayFormatter = DateFormatter()
+//            monthAndDayFormatter.dateFormat = "MM월 dd일"
+//            monthAndDayFormatter.locale = Locale(identifier:"ko_KR")
+//            // 요일 표시 포맷
+//            let dayOfWeekFormatter = DateFormatter()
+//            dayOfWeekFormatter.dateFormat = "EEEE"
+//            dayOfWeekFormatter.locale = Locale(identifier:"ko_KR")
+//            //현재 날짜를 불러올 상수
+//            let today = Date()
+//            // 3년전 날짜 구하는 형식
+//            let threeYearsAgo = Calendar.current.date(byAdding:.year, value: -3, to: today)
+//            // let _ = print("\(threeYearsAgo!.description)")
+//            // 2달 뒤 날짜 구하는 형식
+//            let twoMonthAfter = Calendar.current.date(byAdding:.month, value: 2, to: today)
+//            // let _ = print("\(twoMonthAfter!.description)")
+//            // 비교하기 위해 시작 지점과 끝 지점 계산
+//            // 2021-04-18 06:51:12 +0000 3년전
+//            let beforeDate = DateFormatter(Day).date(from: DateFormatter(Day).string(from: threeYearsAgo!))
+//            // 2024-06-18 06:51:12 +0000 2달 뒤
+//            let afterDate = DateFormatter(Day).date(from: DateFormatter(Day).string(from: twoMonthAfter!))
+//            let interval = afterDate?.timeIntervalSince(beforeDate!)
+//            // 하루 기준으로 표시한 날짜 차이 - 배열의 크기가 되어야함
+//            let daysInterval = Int(interval! / 86400) // 1157
+//            // let _ = print(afterDate)
+//            // 변수 따로 만들어서 데이터를 갱신
+//            var currentDate = Calendar.current.date(byAdding: .year, value: -3, to: today)!
+//            // 제일 과거 날짜를 기준으로 하루씩 늘려가면서 배열에 추가
+//            for _ in 0..<daysInterval {
+//                // Date 배열 저장
+//                DateSetArray.dateArray.append(DateFormatter(Day).string(from: currentDate))
+//                // let _ = print(DateSetArray.dateArray)
+//                // MM월dd 로 데이터 변환
+//                // let _ = print(DateSetArray.dayOfWeek)
+//
+//                // 하루 씩 늘려가면서 날짜를 증가시킴
+//                currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)!
+//            }
+//            let _ = print("\(daysInterval)")
+//            let _ = print("\(DateSetArray.dateArray)")
+//            let _ = print("\(String(describing: threeYearsAgo))")
+//            let _ = print("\(String(describing: twoMonthAfter))")
--- SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/MedicineDaily.swift
+++ SwiftUI essentials/NC1_DailyApp/NC1_DailyApp/View/MedicineDaily.swift
@@ -44,9 +44,7 @@ struct MedicineDaily : View {
                                painDate: Date()-1)]),
     ]// 배열 끝
     //@State var DateArray : DateArray = DateArray(dateArray: [Date], dayOfWeek: [], monthAndDay: [])
-    @State var DateSetArray : DateSetArray
-//    @State var dateArray : [Date] = []
-//    @State var dateIntArray : [Int] = []
+    // @State var DateSetArray : DateSetArray
     @State var ArrayDateAndInt : [(Date, Int)] = []
     // 현재 탭 번호
     @State var currentTab: Int = 0
@@ -56,8 +54,8 @@ struct MedicineDaily : View {
         ZStack(alignment: .top) {
             // 탭뷰는 현재 탭 번호를 받는다.
             TabView(selection: self.$currentTab) {
-                View1(medicines: $medicines, DateSetArray: $DateSetArray).tag(0)
-                View2(data: DateSetArray.dateArray.self, content: {  in
+                View1(medicines: $medicines, DateSetArray: Binding<DateSetArray>).tag(0)
+                View2(data: DateSetArray.dateArray.self, content: { date in
                     <#code#>
                 }).tag(1)
             }
@@ -67,51 +65,7 @@ struct MedicineDaily : View {
             .edgesIgnoringSafeArea(.all)
             TabBarView(currentTab: self.$currentTab)
         }.onAppear(){
-            // 날짜를 문자열로 바꿔주기 위한 작업
-            let DayFormatter = DateFormatter()
-            DayFormatter.dateFormat = "yyyy-MM-dd"
-            // 월 일 표시 포맷
-            let monthAndDayFormatter = DateFormatter()
-            monthAndDayFormatter.dateFormat = "MM월 dd일"
-            monthAndDayFormatter.locale = Locale(identifier:"ko_KR")
-            // 요일 표시 포맷
-            let dayOfWeekFormatter = DateFormatter()
-            dayOfWeekFormatter.dateFormat = "EEEE"
-            dayOfWeekFormatter.locale = Locale(identifier:"ko_KR")
-            // 현재 날짜를 불러올 상수
-            let today = Date()
-            // 3년전 날짜 구하는 형식
-            let threeYearsAgo = Calendar.current.date(byAdding:.year, value: -3, to: today)
-            // let _ = print("\(threeYearsAgo!.description)")
-            // 2달 뒤 날짜 구하는 형식
-            let twoMonthAfter = Calendar.current.date(byAdding:.month, value: 2, to: today)
-            // let _ = print("\(twoMonthAfter!.description)")
-            // 비교하기 위해 시작 지점과 끝 지점 계산
-            // 2021-04-18 06:51:12 +0000 3년전
-            let beforeDate = DayFormatter.date(from: DayFormatter.string(from: threeYearsAgo!))
-            // 2024-06-18 06:51:12 +0000 2달 뒤
-            let afterDate = DayFormatter.date(from: DayFormatter.string(from: twoMonthAfter!))
-            let interval = afterDate?.timeIntervalSince(beforeDate!)
-            // 하루 기준으로 표시한 날짜 차이 - 배열의 크기가 되어야함
-            let daysInterval = Int(interval! / 86400) // 1157
-            // let _ = print(afterDate)
-            // 변수 따로 만들어서 데이터를 갱신
-            var currentDate = Calendar.current.date(byAdding: .year, value: -3, to: today)!
-            // 제일 과거 날짜를 기준으로 하루씩 늘려가면서 배열에 추가
-            for _ in 0..<daysInterval {
-                // Date 배열 저장
-                DateSetArray.dateArray.append(DayFormatter.string(from: currentDate))
-                // let _ = print(DateSetArray.dateArray)
-                // MM월dd 로 데이터 변환
-                // let _ = print(DateSetArray.dayOfWeek)
 
-                // 하루 씩 늘려가면서 날짜를 증가시킴
-                currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)!
-            }
-            let _ = print("\(daysInterval)")
-            let _ = print("\(DateSetArray.dateArray)")
-            let _ = print("\(String(describing: threeYearsAgo))")
-            let _ = print("\(String(describing: twoMonthAfter))")
         }
     }
 }
