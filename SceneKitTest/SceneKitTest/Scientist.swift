0c9428ff43fdb7d57b3a35a3fa33a59d2cfadc02
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-06-17 15:20:45 +0900

    NC2 Test File UPLOAD

---
+++ SceneKitTest/SceneKitTest/Scientist.swift
@@ -0,0 +1,18 @@
+//
+//  Scientist.swift
+//  SceneKitTest
+//
+//  Created by lovehyun95 on 6/13/24.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
+
